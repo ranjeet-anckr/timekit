fe8beef71a083eb9099edc7e33fea1ec1e00ba2f
ranjeet-anckr <ranjeet.kumar@xotiv.com>
2024-06-10 18:33:20 +0530

    button added

--- src/app/(site)/coming-soon/page.tsx
+++ src/app/(site)/coming-soon/page.tsx
@@ -1,29 +1,62 @@
+'use client';
 import React from 'react';
 import Container from '~/core/ui/Container';
-import Image from 'next/image';
 import Button from '~/core/ui/Button';
 import { ChevronLeft } from 'lucide-react';
+import Heading from '~/core/ui/Heading';
+import SubHeading from '~/core/ui/SubHeading';
+import TextField from '~/core/ui/TextField';
 
 const ComingSoon = () => {
   return (
     <div className="flex flex-col overflow-auto">
       <Container>
-        <div className="relative w-full h-screen">
+        <div
+          className="w-full h-screen bg-no-repeat bg-cover bg-center"
+          style={{
+            backgroundImage: `url("/assets/images/yellow background.png")`,
+          }}
+        >
           <Button
             variant="link"
             href="/"
-            className="absolute top-4 left-4 z-10 bg-white font-bold text-lg"
+            className="  bg-customColor text-black font-bold text-lg mt-10 ml-10"
           >
             <ChevronLeft />
             <span className="ml-2">Back to HomePage</span>
           </Button>
-          <Image
-            className="absolute inset-0 w-full h-full "
-            layout="fill"
-            src="/assets/images/background.png"
-            alt="App Image"
-            placeholder="empty"
-          />
+          <div className="flex justify-center ">
+            <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-center h-full p-4 ">
+              <img
+                src="/assets/images/coming soon.png"
+                alt="coming-soon"
+                className="w-96 h-80 object-contain"
+              />
+              <Heading type={2}>
+                Get ready for the ultimate word game experience with Wordoken!
+              </Heading>
+              <SubHeading>
+                Challenge your friends, become a word champion, and enjoy
+                pulse-pounding action! Fun for experts and players of any level.
+                If you like The New York Times Spelling Bee, you&apos;ll love
+                Wordoken. Enjoy full access without any subscription. Don&apos;t
+                miss out - sign up now for exclusive updates!
+              </SubHeading>
+              <div className="flex flex-row gap-2 mt-10">
+                <TextField>
+                  <TextField.Input
+                    required
+                    type="text"
+                    className="uppercase font-bold text-lg  focus-visible:ring-customColor"
+                    placeholder="Enter a email"
+                  />
+                </TextField>
+                <Button className="bg-customColor hover:bg-customColor text-black font-bold text-lg ">
+                  Notify Me !
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
